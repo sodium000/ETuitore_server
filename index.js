6cef999646d45c9f68ee2a1a971efd8cf8dca93c
Raisul Islam Tonmoy <sodiumrdx@gmail.com>
2025-12-10 00:01:13 +0600

    user save in database

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const express = require('express')
+const express = require("express");
 const cors = require("cors");
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { format } = require("date-fns");
 
 // middleware added
 app.use(express.json());
 app.use(cors());
-
 
 // mogodb connection
 const uri = `mongodb+srv://${process.env.db_username}:${process.env.db_password}@chat-application.qhq6ecs.mongodb.net/?appName=chat-application`;
@@ -19,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 async function run() {
   try {
@@ -27,27 +25,33 @@ async function run() {
     const DB = client.db("e_TutionBD");
     const userCollection = DB.collection("users");
 
-
+    // all user
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log("user")
+      user.createdAt = format(new Date(), "yyyy-MM-dd HH:mm:ss");
+      const email = user.email;
+      const userExists = await userCollection.findOne({ email });
+      if (userExists) {
+        return res.send({ message: "user exists" });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
   }
 }
-run()
+run();
 
-
-
-
-
-
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World! etutionBD')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World! etutionBD");
+});
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`)
-})
+  console.log(`http://localhost:${port}`);
+});
